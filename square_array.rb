3475a2a39081a1e09de5e98b15afe3801c77443a
Matthew Lewandowski <matthew.lewandowski@wework.com>
2019-09-06 18:31:19 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,6 @@
 def square_array(numbers)
   numbers = [1,2,3]
   new_numbers = []
-  numbers.each {|x| x ** 2}
-  return numbers 
+  numbers.each {|x| new_numbers << x ** 2}
+  return new_numbers
 end
